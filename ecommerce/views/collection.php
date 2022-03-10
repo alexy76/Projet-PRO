b3975d927ff5c2014002564667984ad73e31049d
Alexy.L <alexy.lepretre76@laposte.net>
2022-03-10 22:25:29 +0100

    Survol des produits / affichage du bouton 'voir'

--- ecommerce/views/collection.php
+++ ecommerce/views/collection.php
@@ -10,34 +10,40 @@ include '../views/templates/header.php';
 <div class="container mt-5">
 
     <div class="card text-center">
-        <!-- <div class="card-header">
-                <ul class="nav nav-tabs card-header-tabs">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="true" href="">Collections</a>
-                    </li>
-                </ul>
-            </div> -->
+
         <div class="card-body">
 
+            <div class="row justify-content-center p-lg-3 p-1">
 
+                <?php if (!$productsByCollection) : ?>
+                    <p>Aucune données trouvées</p>
+                    <?php
+                else :
+                    foreach ($productsByCollection as $key => $product) : ?>
 
-                    <h1 class="card-title mb-5"></h1>
+                        <style>
+                            .card<?= $key ?>:hover .card__overlay {
+                                opacity: 1;
+                            }
+                        </style>
 
-                    <div class="row justify-content-center p-lg-3 p-1">
-
-                    <?php foreach($productsByCollection as $product) : ?>
-
-                        <div class="card col-lg-3 col-12 mb-5" style="border: 0px;">
-                            <img src="../assets/img/products/<?= $product->imageProduct ?>" class="card-img-top" alt="<?= $product->labelImage ?>">
+                        <div class="card card<?= $key ?> cardRelative col-lg-3 col-12 mb-5" style="border: 0px;">
+                            <img src="../assets/img/products/<?= $product->imageProduct ?>" class="card-img-top img-fluid" alt="<?= $product->labelImage ?>">
                             <div class="card-body">
                                 <h2 class="card-title h6"><?= ucfirst(strtolower($product->titleProduct)) ?></h2>
                                 <p class="card-text"></p>
                             </div>
+                            <div class="card__overlay">
+                                <div class="overlay__text">
+                                    <a href="./product.php?id=<?= $product->idProduct ?>&slug=<?= $product->slugProduct ?>" class="btn btn-primary px-5 py-2 fs-5 border border-2 border-white">Voir</a>
+                                </div>
+                            </div>
                         </div>
 
-                    <?php endforeach; ?>
+                <?php endforeach;
+                endif; ?>
 
-                    </div>
+            </div>
 
 
         </div>
